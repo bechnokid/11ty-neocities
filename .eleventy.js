479f7283b9bbaaf5ed7335f00d2327c3f1afe071
Bechno Kid <bechnokid@gmail.com>
2023-08-19 21:31:34 +0000

    Initial commits for blog

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,16 @@
 module.exports = config => {
+  const dateFilter = require('./src/filters/date-filter.js');
+  const w3DateFilter = require('./src/filters/w3-date-filter.js');
+
   config.addPassthroughCopy("./src/css/");
   config.addPassthroughCopy("./src/fonts/");
   config.addPassthroughCopy("./src/images/");
 
+  config.addFilter('dateFilter', dateFilter);
+  config.addFilter('w3DateFilter', w3DateFilter);
+
   config.addCollection('blog', collection => {
-    return [...collection.getFilteredByGlob('./src/writing/*.html')].reverse();
+    return [...collection.getFilteredByGlob('./src/blog/*.html')].reverse();
   });
 
   return {
