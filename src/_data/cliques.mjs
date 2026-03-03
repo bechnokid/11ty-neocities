2ff3de36066dd3477a6064cd8968393031add8c0
Bechno Kid <bechnokid@gmail.com>
2026-03-03 14:42:56 +0000

    clique commits
    - moved clique images to _main folder
    - added "balls" clique
    - change "id" to "cls" for consistency
    - updated clique template

--- src/_data/cliques.mjs
+++ src/_data/cliques.mjs
@@ -1,70 +1,39 @@
 /*
 ## SIMPLE TEXT
-{
-  left: "",
-  center: "",
-  right: ""
-},
+{ left: "", center: "", right: "" },
 
 ## FULL
 {
-  left: {
-    cls: "",
-    url: "",
-    text: "",
-  },
-  center: {
-    url: "",
-    text: "",
-    img: ""
-  },
-  right: {
-    cls: "",
-    text: ""
-  }
+  cls: "(optional)",
+  left: { cls: "", url: "", text: "" },
+  center: { url: "", text: "", img: "" },
+  right: { cls: "", text: "" }
 }
 
 ## CUSTOM
-{
-  custom: "HTML code goes here"
-}
+{ custom: "HTML code goes here" }
 */
+const imgPath = "/assets/images/goodies/cliques/_main/";
 export default {
-  path: "/assets/images/goodies/cliques/",
+  path: imgPath,
   col1: [
     {
-      left: {
-        url: "https://aromatic.wings.nu/",
-        text: "aromatic"
-      },
+      left: { url: "https://aromatic.wings.nu/", text: "aromatic" },
       center: "//",
       right: "bánh bò"
     },
     {
-      left: {
-        cls: "rainbow clique",
-        text: "rainbow",
-        url: "https://cliqued.wings.nu/various.php"
-      },
+      left: { cls: "rainbow clique", text: "rainbow", url: "https://cliqued.wings.nu/various.php" },
       center: "🌈",
-      right: {
-        cls: "rainbow fav",
-        text: "Mint Green"
-      }
+      right: { cls: "rainbow fav", text: "Mint Green" }
     },
     {
-      left: {
-        text: "sanrio",
-        url: "https://nanacore.neocities.org/cliques/sanrio"
-      },
+      left: { text: "sanrio", url: "https://nanacore.neocities.org/cliques/sanrio" },
       center: "♡",
       right: "Big Challenges"
     },
     {
-      left: {
-        url: "https://cliques.windsprite.nu/mine/",
-        text: "Mine!"
-      },
+      left: { url: "https://cliques.windsprite.nu/mine/", text: "Mine!" },
       center: "//",
       right: "Gilgamesh (FFV)"
     },
@@ -73,30 +42,17 @@ export default {
         text: "flowers",
         cls: "flower-clique",
       },
-      center: {
-        text: "ꕤ",
-        url: "https://reef.kingdra.net",
-      },
-      right: "hydrangeas <a href='https://web.archive.org/web/20250621102731/http://jbrr.fc2web.com/material.html'><img class='ms-1 inline-img' src='/assets/images/goodies/cliques/hydrangea.gif' aria-hidden='true' alt=''></a>",
+      center: { text: "ꕤ", url: "https://reef.kingdra.net" },
+      right: `hydrangeas <a href='https://web.archive.org/web/20250621102731/http://jbrr.fc2web.com/material.html'><img class='ms-1 inline-img' src=${imgPath + 'hydrangea.gif'} aria-hidden='true' alt=''></a>`,
     },
     {
-      left: {
-        text: "Crystallizing",
-        url: "https://crystallizing.neocities.org"
-      },
-      center: {
-        img: "phos.png",
-      },
+      left: { text: "Crystallizing", url: "https://crystallizing.neocities.org" },
+      center: { img: "phos.png", },
       right: "Phosphophyllite"
     },
     {
-      left: {
-        text: "my jam!!",
-        url: "https://cliqued.wings.nu/",
-      },
-      center: {
-        img: "jam.png",
-      },
+      left: { text: "my jam!!", url: "https://cliqued.wings.nu/" },
+      center: { img: "jam.png" },
       right: "Chaotic Love Revolution"
     },
   ],
@@ -105,15 +61,11 @@ export default {
       {
         name: "Dearest",
         tooltip: "OTP: Ryuuko Matoi and Senketsu",
-        id: "dearest-otp",
+        cls: "dearest-otp",
         url: "https://dearest.snow-heart.net",
         img: "otp_dearest.png",
         alt: "A pink button with hearts on the side and text that says, 'OTP Dearest'",
-        mobile: {
-          left: "OTP",
-          center: "::",
-          right: "Ryuuko Matoi & Senketsu"
-        }
+        mobile: { left: "OTP", center: "::", right: "Ryuuko Matoi & Senketsu" }
       },
       {
         name: "Caffeine NATION",
@@ -121,33 +73,21 @@ export default {
         url: "https://caffeinated.wings.nu/index.php",
         img: "caffeine-nation.png",
         alt: "A button that says, 'Caffeine NATION'",
-        mobile: {
-          left: "caffeineNATION",
-          center: "☕︎",
-          right: "Light roast with creamer"
-        }
+        mobile: { left: "caffeineNATION", center: "☕︎", right: "Light roast with creamer" }
       },
       {
         name: "Astrology",
         url: "https://dust.kuchiki.net/",
         img: "aquarius.gif",
         alt: "A button with a star and text that says, 'Aquarius'",
-        mobile: {
-          left: "celestial divine",
-          center: "//",
-          right: "aquarius"
-        }
+        mobile: { left: "celestial divine", center: "//", right: "aquarius" }
       },
       {
         name: "Anime Genres",
         url: "https://dust.kuchiki.net/",
         img: "shoujo.gif",
         alt: "A pink button with a floating heart and text that says, 'Shoujo'",
-        mobile: {
-          left: "i love anime",
-          center: "//",
-          right: "shoujo"
-        }
+        mobile: { left: "i love anime", center: "//", right: "shoujo" }
       },
       {
         name: "Barbieland",
@@ -155,11 +95,7 @@ export default {
         url: "https://fan.sanguineroyal.com/cli/barbieland/",
         img: "barbieland.gif",
         alt: "A white button with pink text that says, \"Barbieland\" with the Barbie logo on the side",
-        mobile: {
-          left: "Barbieland",
-          center: "::",
-          right: "This Barbie is a Monster Hunter!"
-        }
+        mobile: { left: "Barbieland", center: "::", right: "This Barbie is a Monster Hunter!" }
       },
       {
         name: "Stardaze",
@@ -167,18 +103,14 @@ export default {
         url: "https://star.aquarel.nu/",
         img: 'stardaze.gif',
         alt: 'A pink white and purple starry button that says, \"Stardaze\"',
-        mobile: {
-          left: 'Stardaze',
-          center: '☆',
-          right: "Wezen"
-        }
+        mobile: { left: 'Stardaze', center: '☆', right: "Wezen" }
       }
     ],
     [
       {
         name: "Pretty Guardians",
         tooltip: "Under the protection of Mercury",
-        id: "mercury",
+        cls: "mercury",
         url: "https://sailorcrystal.net/guardians/",
         img: "mercury.png",
         alt: "Sailor Mercury",
@@ -186,7 +118,7 @@ export default {
       },
       {
         name: "Final Fantasy Moogle Adoption Clique",
-        id: "moogle",
+        cls: "moogle",
         tooltip: "I ♥ moogles // Kupo, kupo! Moogle Adoption Clique",
         url: "https://midnight-cloud.net/clique/moogle/",
         img: "moogle.gif",
@@ -194,7 +126,7 @@ export default {
       },
       {
         name: "30+ Club",
-        id: "30plus",
+        cls: "30plus",
         url: "https://moonpr1sm.com/random/30plusclub",
         img: "30plusclub.png",
         alt: "Button that says \"30+ Club\"",
@@ -202,7 +134,7 @@ export default {
       },
       {
         name: 'pixel friends',
-        id: 'pixel-friend',
+        cls: 'pixel-friend',
         tooltip: "pixel friends | Felyne",
         url: 'https://ac.kuchiki.net/',
         img: 'felyne.gif',
@@ -210,16 +142,12 @@ export default {
       },
       {
         name: "I Choose You!",
-        id: "i-choose-you",
+        cls: "i-choose-you",
         tooltip: "I choose you! :: CHI-YU",
         url: "https://pkmn.aquarels.net/",
         img: "i-choose-you.gif",
         alt: "The pokemon Chi-Yu",
-        mobile: {
-          left: "I Choose You!",
-          center: "|",
-          right: "CHI-YU"
-        }
+        mobile: { left: "I Choose You!", center: "|", right: "CHI-YU" }
       },
       {
         name: "Adopt a Boba",
@@ -243,11 +171,7 @@ export default {
         url: "https://ff.aquarel.nu/",
         img: "x-gilgamesh.png",
         alt: "Gilgamesh as he appears in Final Fantasy Brave Exvius",
-        mobile: {
-          left: "Exvius",
-          center: "//",
-          right: "Gilgamesh"
-        }
+        mobile: { left: "Exvius", center: "//", right: "Gilgamesh" }
       },
       {
         name: "Grand Summoning",
@@ -255,216 +179,127 @@ export default {
         url: "https://deathbusters.org/aeon/",
         img: "yojimbo.png",
         alt: "Yojimbo as he appears in Final Fantasy Brave Exvius",
-        mobile: {
-          left: "Grand Summoning",
-          center: "::",
-          right: "Yojimbo"
-        }
+        mobile: { left: "Grand Summoning", center: "::", right: "Yojimbo" }
       }
     ]
   ],
   more: [
     {
-      left: {
-        text: 'Cutie Connection',
-        url: 'https://lockheart.love/collections/cliques',
-      },
-      center: {
-        img: 'paw-purple.png',
-      },
+      left: { text: 'Cutie Connection', url: 'https://lockheart.love/collections/cliques', },
+      center: { img: 'paw-purple.png' },
       right: 'Dodogama'
     },
     {
-      left: {
-        url: "https://emocowboy.neocities.org/?emo=/home/cliques/pokemon",
-        text: "favorite",
-      },
-      center: {
-        img: "pokefav.png"
-      },
+      left: { url: "https://emocowboy.neocities.org/?emo=/home/cliques/pokemon", text: "favorite" },
+      center: {img: "pokefav.png"},
       right: "Chi-yu"
     },
     {
-      left: {
-        url: "https://cliqued.wings.nu/fandom.php",
-        text: "OTF",
-      },
-      center: {
-        img: "otf.png"
-      },
+      left: { url: "https://cliqued.wings.nu/fandom.php", text: "OTF" },
+      center: { img: "otf.png" },
       right: "Digimon"
     },
     {
-      left: {
-        url: "https://reef.kingdra.net/",
-        text: "greenery",
-      },
-      center: {
-        img: "greenery.png",
-      },
+      left: { url: "https://reef.kingdra.net/", text: "greenery" },
+      center: { img: "greenery.png" },
       right: "cacti"
     },
     {
-      left: {
-        url: "https://reef.kingdra.net/",
-        text: "gemstone",
-      },
-      center: {
-        img: "gemstone.png",
-      },
+      left: { url: "https://reef.kingdra.net/", text: "gemstone" },
+      center: { img: "gemstone.png" },
       right: "amethyst"
     },
     {
-      left: {
-        url: "https://lazer-bunny.neocities.org/Cliques",
-        text: "Crispy",
-      },
-      center: {
-        img: "crispy.png",
-      },
+      left: { url: "https://lazer-bunny.neocities.org/Cliques", text: "Crispy" },
+      center: { img: "crispy.png" },
       right: "Silent Salt Cookie"
     },
     {
-      left: {
-        url: "https://cliques.shadow-lord.com/deadlysins/buttons.php",
-        text: "Wrath",
-      },
+      left: { url: "https://cliques.shadow-lord.com/deadlysins/buttons.php", text: "Wrath" },
       right: " is my Deadly Sin"
     },
     {
-      left: {
-        url: "https://lazer-bunny.neocities.org/Cliques",
-        text: "My OTP",
-      },
-      center: {
-        img: "my-otp.png",
-      },
+      left: { url: "https://lazer-bunny.neocities.org/Cliques", text: "My OTP" },
+      center: { img: "my-otp.png" },
       right: "Siduri x Gilgamesh (FF7R)"
     },
     {
-      custom: "<a href='https://joroki.neocities.org/cliques'>this site is <img class='inline-img' src='/assets/images/goodies/cliques/camp-cute.png' alt='cute'></a>",
+      custom: `<a href='https://joroki.neocities.org/cliques'>this site is <img class='inline-img' src=${imgPath + "camp.png"} alt='cute'></a>`,
     },
     {
-      left: {
-        url: "https://joroki.neocities.org/cliques",
-        text: "my usual",
-      },
+      left: { url: "https://joroki.neocities.org/cliques", text: "my usual" },
       center: "//",
-      right: "iced caramel latte <img src='/assets/images/goodies/cliques/usual.png' alt='' aria-hidden='true'>",
+      right: `iced caramel latte <img src=${imgPath + 'usual.png'} alt='' aria-hidden='true'>`,
     },
     {
-      left: {
-        url: "https://petrapixel.neocities.org/cliques",
-        text: "love tropes",
-      },
+      left: { url: "https://petrapixel.neocities.org/cliques", text: "love tropes" },
       center: "//",
       right: "slowburn friends to lovers"
     },
     {
-      left: {
-        url: "https://petrapixel.neocities.org/cliques",
-        text: "youtube",
-      },
+      left: { url: "https://petrapixel.neocities.org/cliques", text: "youtube" },
       center: "//",
       right: "Caddicarus"
     },
     {
       left: "Ambient Wonder",
       center: "//",
-      right: {
-        url: "https://bloodgulch.neocities.org/ambientwonder/codes",
-        text: "Pokke Village (MHGU)",
-      },
+      right: { url: "https://bloodgulch.neocities.org/ambientwonder/codes", text: "Pokke Village (MHGU)" },
     },
     {
-      left: {
-        url: "https://key.aquarels.net/",
-        text: "Keyblade"
-      },
-      center: {
-        img: "keyblade.png",
-      },
+      left: { url: "https://key.aquarels.net/", text: "Keyblade" },
+      center: { img: "keyblade.png" },
       right: "Guardian's Soul"
     },
     {
-      left: {
-        url: "https://patron.snow-heart.net/",
-        text: "patron",
-      },
+      left: { url: "https://patron.snow-heart.net/", text: "patron" },
       center: "::",
       right: "Numemon"
     },
     {
-      left: {
-        url: "https://xandra.cc/patrons",
-        text: "protector"
-      },
+      left: { url: "https://xandra.cc/patrons", text: "protector" },
       center: "🌊",
       right: "Neptune(mon)"
     },
     {
       left: "Gilgamesh",
-      center: {
-        img: "materia.png",
-      },
-      right: {
-        text: "materia",
-        url: "https://www.garden.cainhurst.org/",
-      }
+      center: { img: "materia.png" },
+      right: { text: "materia", url: "https://www.garden.cainhurst.org/" }
     },
     {
-      left: {
-        text: "Cringe is Dead",
-        url: "https://lockheart.love/collections/cliques",
-      },
+      left: { text: "Cringe is Dead", url: "https://lockheart.love/collections/cliques" },
       center: "||",
       right: "OC x Canon"
     },
     {
-      left: {
-        text: "Internet Obsession",
-        url: "https://lockheart.love/collections/cliques",
-      },
+      left: { text: "Internet Obsession", url: "https://lockheart.love/collections/cliques" },
       center: "||",
       right: "Small pixel adoptables"
     },
     {
-      id: "type-master",
-      custom: "<img src='/assets/images/goodies/cliques/dark.png'> I am a <span class='font-weight-bold'>dark</span> type <a href='https://seafare.neocities.org/cliques/'>master</a>! <img src='/assets/images/goodies/cliques/dark.png'>",
+      cls: "type-master",
+      custom: `<img src=${imgPath + 'dark.png'}> I am a <span class='font-weight-bold'>dark</span> type <a href='https://seafare.neocities.org/cliques/'>master</a>! <img src=${imgPath + 'dark.png'}>`,
     },
     {
-      left: {
-        text: "support",
-        url: "https://reef.kingdra.net",
-      },
-      center: {
-        img: "splatoon.png",
-      },
+      left: { text: "support", url: "https://reef.kingdra.net" },
+      center: { img: "splatoon.png" },
       right: "heavy splatling"
     },
     {
-      left: {
-        text: "<strike>un</strike>problematic blorbo",
-        cls: "problematic",
-      },
-      center: {
-        text: "❥",
-        url: "https://reef.kingdra.net",
-      },
-      right: {
-        text: "dr. xeno",
-        cls: "blorbo",
-      }
+      left: { text: "<strike>un</strike>problematic blorbo", cls: "problematic" },
+      center: { text: "❥", url: "https://reef.kingdra.net" },
+      right: { text: "dr. xeno", cls: "blorbo" }
     },
     {
-      left: {
-        text: "LINKED!",
-        url: "https://ballonlea.net/trickyfox/",
-      },
-      center: {
-        img: "linked.png",
-      },
+      left: { text: "LINKED!", url: "https://ballonlea.net/trickyfox/" },
+      center: { img: "linked.png" },
       right: "Miss Kobayashi"
+    },
+    {
+      cls: "balls",
+      left: { text: "balls", url: "https://icirr.us/balls/" },
+      center: { img: "balls.png" },
+      right: "master"
     }
   ],
 }
